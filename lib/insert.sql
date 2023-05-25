c50989294e62f672419842582180df742f8854b5
Sofia Katzman <katzmansof@gmail.com>
2023-05-25 00:22:16 -0400

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, "M", "brown", "mild", 1);
